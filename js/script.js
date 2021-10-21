87b440422828872eb5abf64f26508c4dc3a93aa6
purpledev1337 <88691096+purpledev1337@users.noreply.github.com>
2021-10-21 13:56:27 +0200

    simple version working in console

--- js/script.js
+++ js/script.js
@@ -1,9 +1,35 @@
 // Chiedere all’utente il numero di chilometri da percorrere
+const tripKms = parseInt(prompt("How many kilometres do you have to travel?"));
+console.log("Kms: ", tripKms);
 
 // Chiedere all’utente l’età del passeggero
+const userAge = parseInt(prompt("What's your age?"));
+console.log("Age: ", userAge);
+
 
 // Il prezzo del biglietto è 0.21 € al km
+let basicPrice = tripKms * 0.20;
+console.log("Basic Price: " , basicPrice);
+
 // va applicato uno sconto del 20% per gli under 18
 // va applicato uno sconto del 40% per gli over 65
+let discount;
+if (userAge < 18) {
+    discount = (basicPrice * 20) / 100;
+} else if (userAge > 65) {
+    discount = (basicPrice * 30) / 100;
+} else {
+    discount = 0;
+}
+
+let finalPrice;
+if (discount > 0) {
+    finalPrice = basicPrice - discount
+} else {
+    finalPrice = basicPrice
+}
+
+console.log("Discount%: " , discount)
+console.log("Final Price: " , finalPrice)
 
 // Output del prezzo finale in forma umana (massimo due decimali)
