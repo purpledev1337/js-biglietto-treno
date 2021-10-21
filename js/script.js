9ef22de8ac1de113c3489b02b9beb81f293e4d71
purpledev1337 <88691096+purpledev1337@users.noreply.github.com>
2021-10-21 13:14:19 +0200

    file upload and commented script steps

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// Chiedere all’utente il numero di chilometri da percorrere
+
+// Chiedere all’utente l’età del passeggero
+
+// Il prezzo del biglietto è 0.21 € al km
+// va applicato uno sconto del 20% per gli under 18
+// va applicato uno sconto del 40% per gli over 65
+
+// Output del prezzo finale in forma umana (massimo due decimali)
